f7d44dc3c9553e683da24dab9737d89aada01391
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2023-11-23 03:15:16 +0530

    initiliased frontend

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,6 @@ export default async function connectDB() {
     try{
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Successfully connected to MongoDB");
-        console.log(mongoose.connection.readyState);
     }
     catch(err){
         console.log(err.messge);
