3ea1fdedba0e035fe83cc7ec1b4484bf39bcc0ee
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2023-11-23 17:34:37 +0530

    minor fixes

---
+++ client/app/components/Login.jsx
@@ -0,0 +1,117 @@
+'use client'
+
+import React, { useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { RiEyeCloseLine } from "react-icons/ri";
+
+
+function Loginpage({ setShowLoginModal }) {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    let name, value;
+    const handleChange = (e) => {
+        name = e.target.name;
+        value = e.target.value;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+
+    };
+
+    const [show, setShow] = useState(false);
+    const showPassword = () => {
+        setShow(!show);
+    };
+    return (
+        <>
+        <div className="h-screen w-screen z-20 bg-black fixed inset-0 bg-opacity-50">
+
+            <div className="flex items-center justify-center h-full w-full">
+
+            
+            <div className=" md:w-[350px] lg:w-[500px] bg-white rounded-2xl px-5 py-5  max-[400px] shadow-2xl">
+                <h1 className="font-poppins text-2xl lg:text-4xl font-bold pb-5">
+                    Login to your account
+                </h1>
+                <p className="font-poppins lg:text-xl  pb-4">
+                    Welcome back! Please enter your credentials to access your
+                    account.
+                </p>
+                <div className="my-2">Username or Email</div>
+                <div className="flex flex-row border-gray-400 placeholder:text-[#667086] placeholder:text-[16px]  border-[0.5px] mt-2 rounded-[5px]">
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Enter your email or username"
+                        className="w-full p-2 px-6 py-2 rounded placeholder:text-[#667086]"
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div className="mt-3">Password</div>
+                <div className="flex flex-row items-center border-gray-400 placeholder:text-[#667086] placeholder:text-[16px]  border-[0.5px] mt-2 rounded-[5px]">
+                    <input
+                        type={show ? "text" : "password"}
+                        name="password"
+                        placeholder="Enter your Password"
+                        className="w-full p-2 px-6 py-2 rounded placeholder:text-[#667086]"
+                        width="90vw"
+                        onChange={handleChange}
+                        required
+                    />
+                    {!show ?
+                        <FaEye className="text-3xl pr-2 hover:cursor-pointer"
+                            onClick={() => {
+                                showPassword(!show)
+                            }}
+                        /> :
+                        <RiEyeCloseLine className="text-3xl pr-2 hover:cursor-pointer"
+                            onClick={() => {
+                                showPassword(!show)
+                            }
+                        }
+                        />
+
+                    }
+                </div>
+                <div className="w-full flex gap-10 items-center justify-center mt-4">
+                    <button
+                        className="p-[10px] md:py-[16px] md:px-[16px] text-white bg-[#2270E2] mb-4 font-medium rounded-lg hover:scale-105 transform transition-all"
+                        onClick={handleSubmit}
+                    >
+                        Sign In
+                    </button>
+                    <button className="p-[10px] md:py-[16px] md:px-[16px] text-white bg-red-600 mb-4 font-medium rounded-lg hover:scale-105 transform transition-all"
+                        onClick={
+                            () => {
+                                setShowLoginModal(false)
+                            }
+                        }
+                        >
+                        Cancel
+                    </button>
+
+                </div>
+
+
+
+                <div className=" text-center p-8">
+                    {" "}
+                    Don’t have an account?{" "}
+                    <span className=" hover:cursor-pointer hover:scale-125 transform transition-all text-[#2270E2]"> Sign up</span>
+
+                </div>
+                {/* </div> */}
+            </div>
+            </div>
+                        </div>
+        </>
+    );
+}
+
+export default Loginpage;
--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -1,60 +1,67 @@
+'use client'
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoClose } from "react-icons/io5";
-import { useRef } from "react";
-
+import Loginpage from "./Login";
 export default function Navbar() {
-    var show:Boolean = false;
+    const [show, setShow] = useState(false);
+    const [showLoginModal, setShowLoginModal] = useState(false);
 
     return (
         <>
-             <>
-            <div className='flex justify-center pb-10 items-center'>
-                <div className='max-w-7xl w-full pt-5 px-7 flex items-center justify-between'>
-                    <div className='flex items-center font-bold gap-8'>
-                        <div className='text-[32px] hover:cursor-pointer'>
-                            horizon
-                        </div>
+            <>
+                <div className='flex justify-center pb-10 items-center'>
+                    <div className='max-w-7xl w-full pt-5 px-7 flex items-center justify-between'>
+                        <div className='flex items-center font-bold gap-8'>
+                            <div className='text-[32px] hover:cursor-pointer'>
+                                horizon
+                            </div>
 
-                        <div className='md:flex gap-5 hidden text-[13px]'>
-                            <p className="hover:cursor-pointer">Cybersecurity</p>
-                            <p className="hover:cursor-pointer">Blog</p>
-                            <p className="hover:cursor-pointer">Marketing</p>
-                            <p className="hover:cursor-pointer">Content Making</p>
-                            <p className="hover:cursor-pointer">More blogs</p>
+                            <div className='md:flex gap-5 hidden text-[13px]'>
+                                <p className="hover:cursor-pointer">Colleges</p>
+                                <p className="hover:cursor-pointer">Events</p>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <button className='bg-[#2270E2] text-xl hidden md:flex font-semibold text-white px-[18px] py-[8px] rounded-lg'                        >
-                            Login
-                        </button>
-                        {!show ? <GiHamburgerMenu id='open' className='md:hidden text-[32px] hover:cursor-pointer'/> 
-                            :<IoClose id='close' className='md:hidden text-[42px] hover:cursor-pointer'/>}
+                        <div>
+                            <button className='bg-[#2270E2] text-xl hidden md:flex font-semibold text-white px-[18px] py-[8px] rounded-lg'       
+                            onClick={() => setShowLoginModal(!showLoginModal)}
+                            >
+                                Login
+                            </button>
+                            {!show ?
+                                <GiHamburgerMenu id='open' className='md:hidden text-[32px] hover:cursor-pointer'
+                                    onClick={() => setShow(!show)}
+                                />
+                                : <IoClose id='close' className='md:hidden text-[42px] hover:cursor-pointer'
+                                    onClick={() => setShow(!show)}
+                                />}
+                        </div>
                     </div>
                 </div>
-            </div>
-             {show&&<div className="absolute md:hidden  z-20 bg-white font-bold text-[16px] gap-3 w-screen flex flex-col py-3 pr-5 items-end justify-center">
-                <p className="hover:cursor-pointer">Cybersecurity</p>
-                <p className="hover:cursor-pointer">Blog</p>
-                <p className="hover:cursor-pointer">Marketing</p>
-                <p className="hover:cursor-pointer">Content Making</p>
-                <p className="hover:cursor-pointer">More blogs</p>
-                <p className="hover:cursor-pointer">
-                    <p className="bg-[#2270E2] text-white font-bold py-[12px] px-[24px] rounded-2xl" >
-                        Login
+                {show && <div className="absolute md:hidden  z-20 bg-white font-bold text-[16px] gap-3 w-screen flex flex-col py-3 pr-5 items-end justify-center">
+                    <p className="hover:cursor-pointer">Colleges</p>
+                    <p className="hover:cursor-pointer">Events</p>
+                    <p className="hover:cursor-pointer">
+                        <p className="bg-[#2270E2] text-white font-bold py-[12px] px-[24px] rounded-2xl" 
+                        onClick={() => setShowLoginModal(!showLoginModal)}
+                        >
+                            Login
+                        </p>
                     </p>
-                </p>
-                <p className="hover:cursor-pointer">
-                    <button className="bg-[#2270E2] text-white font-bold py-[12px] px-[24px] rounded-2xl"
-                    >
-                        Register
-                    </button>
-                </p>
+                    <p className="hover:cursor-pointer">
+                        <button className="bg-[#2270E2] text-white font-bold py-[12px] px-[24px] rounded-2xl"
+                        >
+                            Register
+                        </button>
+                    </p>
 
-            </div>}
+                </div>}
+                {
+                    showLoginModal&&<Loginpage setShowLoginModal={setShowLoginModal}/>
+                }
 
-            
-            
-        </>
+
+            </>
         </>
     )
 
